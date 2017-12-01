5b5c25fb6ea7be8902a6dd05be3786e048c414d8
张旭 <xychq8@gmail.com>
2017-12-01 15:56:17 +0800

    update
    update

--- feign/src/main/java/com/github/xychq8/feign/FeignApplication.java
+++ feign/src/main/java/com/github/xychq8/feign/FeignApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class FeignApplication {
 
 	public static void main(String[] args) {
